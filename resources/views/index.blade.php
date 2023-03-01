daea2d9b808a6814b90d7c8b6eb8eae9d3ba614f
Natalialra <lra.natalia@yahoo.com.br>
2023-03-01 20:47:55 -0300

    criacao-body

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,25 @@
 @extends ('layouts.app')
 
 @section('body')
-    <h1>Hello World</h1>
+    <div class="jumbotron bg-light border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Produtos</h5>
+                        <p class="card-text">Alguma Coisa</p>
+                        <a href="/produtos" class="btn btn-primary">Cadastre seus produtos</a>
+                    </div>
+                </div>
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Categorias</h5>
+                        <p class="card-text">Alguma Coisa</p>
+                        <a href="/produtos" class="btn btn-primary">Cadastre seus Categorias</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
