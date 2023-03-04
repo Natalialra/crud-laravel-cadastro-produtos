d394dc4672df9f06d245d2cfccd28574a6acab2a
Natalialra <lra.natalia@yahoo.com.br>
2023-03-04 15:06:52 -0300

    criando-rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorProduto;
+use App\Http\Controllers\ControladorCategorias;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/produtos', [ControladorProduto::class, 'index'] );
+
+Route::get('/categorias', [ControladorCategorias::class, 'index'] );
