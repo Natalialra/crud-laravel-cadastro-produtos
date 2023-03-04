15e61a4ce9938ce3f28f68dcb768002eb8cbc27b
Natalialra <lra.natalia@yahoo.com.br>
2023-03-04 14:02:01 -0300

    atualizacao-componentes-front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,23 +3,25 @@
 @section('body')
     <div class="jumbotron bg-light border-secondary">
         <div class="row">
-            <div class="card-deck">
-                <div class="card border border-primary">
-                    <div class="card-body">
-                        <h5 class="card-title">Cadastro de Produtos</h5>
-                        <p class="card-text">Alguma Coisa</p>
-                        <a href="/produtos" class="btn btn-primary">Cadastre seus produtos</a>
-                    </div>
+            <div class="col-sm-6 mb-3 mb-sm-0">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Cadastro de Produtos</h5>
+                  <p class="card-text">Cadastre aqui seus produtos</p>
+                  <a href="/produtos" class="btn btn-primary">Cadastrar Produtos</a>
                 </div>
-                <div class="card border border-primary">
-                    <div class="card-body">
-                        <h5 class="card-title">Cadastro de Categorias</h5>
-                        <p class="card-text">Alguma Coisa</p>
-                        <a href="/produtos" class="btn btn-primary">Cadastre seus Categorias</a>
-                    </div>
-                </div>
+              </div>
             </div>
-        </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Cadastro de Categorias</h5>
+                  <p class="card-text">Cadastre aqui as categorias</p>
+                  <a href="/categorias" class="btn btn-success">Cadastrar Categorias</a>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
 
 @endsection
