e9cc16265af63f493100afdf47ac8c4edd27f1bf
Natalialra <lra.natalia@yahoo.com.br>
2023-03-04 15:07:18 -0300

    criando-views

---
+++ resources/views/categorias.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('body')
+<h4>Página de Categorias</h4>
+@endsection
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('body')
+<h4>Página de Produtos</h4>
+@endsection
