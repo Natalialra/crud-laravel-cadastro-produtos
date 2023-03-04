6b86b1affff39b73c328815d875c2cd5724f6906
Natalialra <lra.natalia@yahoo.com.br>
2023-03-04 15:06:30 -0300

    criando-tabela

--- database/migrations/2023_03_04_170035_create_produtos_table.php
+++ database/migrations/2023_03_04_170035_create_produtos_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 50);
+            $table->integer('estoque');
+            $table->float('preco');
+            $table->unsignedBigInteger('categoria_id'); // cria a referencia para a chave estrangeira
+            $table->foreign('categoria_id')->references('id')->on('categorias'); // cria a chave estrangeira na tabela produtos que referencia a coluna id da tabela categorias
             $table->timestamps();
         });
     }
