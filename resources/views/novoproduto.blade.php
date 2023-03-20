d68857437b5e206a5ceef448f45f19870d35d861
Natalialra <lra.natalia@yahoo.com.br>
2023-03-20 20:34:37 -0300

    criacao-componente-novo-produto

---
+++ resources/views/novoproduto.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app', ["current" => "produtos"] )
+
+@section('body')
+
+    <div class="card">
+        <h5 class="card-header">Adicionar Novo Produto</h5>
+        <div class="card-body">
+            <form action="{{ route('produtos.store') }}" method="POST">
+                @method('POST')
+                @csrf
+                <div class="form-group ">
+                    <div class="row ">
+                        <div class="col-5">
+                            <label for="nomeProduto" class="ms-2">Nome do Produto</label>
+                            <input type="text" name="nomeProduto" class="form-control mt-3" id="nomeProduto">
+                        </div>
+                        <div class="col-2">
+                            <label for="precoProduto" class="ms-2">Preço</label>
+                            <input type="number" name="precoProduto" class="form-control mt-3" id="precoProduto">
+                        </div>
+                        <div class="col-5">
+                            <label for="categoriaProduto" class="ms-2">Categoria</label>
+                        
+                            <select name="categoria_id" class="form-select mt-3" id="categoria_id">
+                                <option selected>Selecione a Categoria</option>
+                                @foreach ($cats as $cat)
+                                    <option value="{{ $cat->id }}">{{ $cat->nome }}</option>
+                                @endforeach
+                            </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-4">Salvar</button>
+                <button type="cancel" class="btn btn-danger mt-4">Cancelar</button>
+            </form>
+        </div>
+    </div>
+@endsection
