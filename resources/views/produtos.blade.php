dd3f4b4f189d1790fcd3434b1e189ada1dab2949
Natalialra <lra.natalia@yahoo.com.br>
2023-03-20 20:34:13 -0300

    criacao-componente-produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app', ["current" => "produtos"] )
 
 @section('body')
-<h4>Página de Produtos</h4>
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Produtos</h5>
+            @if(count($produtos) > 0)
+                <table class="table table-ordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Produto</th>
+                            <th>Preço</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->id }}</td>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->preco }}</td>
+                                <td>
+                                    <a href="/produtos/editar/{{ $produto->id }}" class="btn btn-sm btn-primary">Editar</a>
+                                    <a href="/produtos/apagar/{{ $produto->id }}" class="btn btn-sm btn-danger">Apagar</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+        <div class="card-footer">
+            <a href="/produtos/novo" class="btn btn-sm btn-primary" role="button">Novo Produto</a>
+        </div>
+    </div>
 @endsection
