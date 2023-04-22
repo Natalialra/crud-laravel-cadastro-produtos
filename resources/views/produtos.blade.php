a4457d50c97a71ceb47bfdd39e49d7a6daf82843
Natalialra <lra.natalia@yahoo.com.br>
2023-04-22 16:57:30 -0300

    criando-modal-cadastro-produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app', ["current" => "produtos"] )
+@extends('layouts.app', ['current' => 'produtos'])
 
 @section('body')
-<div class="card border">
-    <div class="card-body">
-        <h5 class="card-title">Cadastro de Produtos</h5>
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Cadastro de Produtos</h5>
             <table class="table table-ordered table-hover">
                 <thead>
                     <tr>
@@ -18,9 +18,52 @@
                 <tbody>
                 </tbody>
             </table>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-sm btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#dlgProdutos">Novo Produto</button>
+        </div>
     </div>
-    <div class="card-footer">
-       <button class="btn btn-sm btn-primary" role="button">Novo Produto</button>
+    <div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form class="form-horizontal" id="formProduto">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Novo Produto</h5>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="id" class="form-control">
+                        <div class="form-group">
+                            <label for="nomeProduto" class="control-label">Nome</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="precoProduto" class="control-label">Preço</label>
+                            <div class="input-group">
+                                <input type="number" class="form-control" id="precoProduto" placeholder="Preço">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="quantidadeProduto" class="control-label">Quantidade </label>
+                            <div class="input-group">
+                                <input type="number" class="form-control" id="quantidadeProduto" placeholder="Quantidade">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="nomeProduto" class="control-label">Categoria</label>
+                            <div class="input-group">
+                                <select id="departamentoProduto" class="form-control">
+
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                        <button type="cancel" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>{{-- data-dismiss faz o dialog fechar quando clica no cancelar  --}}
+                </form>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
